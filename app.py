84bf90aab4b36b0815b747bc53f27659cc390d36
manolisfcb <mmedinac26@gmail.com>
2024-08-21 16:47:03 -0300

    chore: Add Flask form and database integration

--- app.py
+++ app.py
@@ -1,7 +1,55 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, flash
+from flask_wtf import FlaskForm
+from wtforms import StringField, SubmitField
+from wtforms.validators import DataRequired
+
+from flask_sqlalchemy import SQLAlchemy
+from datetime import datetime
+from flask_migrate import Migrate
+# Create db instance
+db = SQLAlchemy()
+
+
+
 
 
 app = Flask(__name__)
+
+
+# Create a secret key for CSRF protection
+app.config['SECRET_KEY'] = 'secret'
+
+
+# Create a form class
+class NameForm(FlaskForm):
+    name = StringField('What is your name?', validators=[DataRequired()])
+    submit = SubmitField('Submit')
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.errorhandler(404)
+def page_not_found(e):
+    return render_template('404.html'), 404
+
+@app.errorhandler(500)
+def internal_server_error(e):
+    return render_template('500.html'), 500
+
+
+@app.route('/name', methods=['GET', 'POST'])
+def name():
+    name = None
+    form = NameForm()
+    if form.validate_on_submit():
+        name = form.name.data
+        form.name.data = ''
+        flash('Form submitted successfully')
+                
+    return render_template('name.html', name=name, form=form)
+
+
 
 
 
